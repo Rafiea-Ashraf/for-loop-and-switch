24aaea2a8019f74368c1bb421bc854d652923d16
Rafiea-Ashraf <rafiea.ashraf@gmail.com>
2024-01-10 14:53:54 +0500

    new

---
+++ for-loop-and-switch.php
@@ -0,0 +1,40 @@
+<?php
+	$number = 4;
+	
+	switch($number){
+	  case '1':
+	  echo 'Number is one';
+	  break;
+	  case '2':
+	  echo 'Number is two';
+	  break;
+	  case '3':
+	  echo 'Number is three';
+	  break;
+	  case '4':
+	  echo 'Number is four';
+	  break;
+	  case '5':
+	  echo 'Number is five';
+	  break;
+	  case '6':
+	  echo 'Number is six';
+	  break;
+	} 
+?>
+
+<?php
+	$number = 2;
+	
+	if ($number == 1){
+	  echo 'Number is One';}
+	 elseif($number == 2){
+	   echo 'number is Two';
+	 }
+	 elseif($number == 3){
+	   echo 'Number is Three';
+	 } 
+	else {
+	  echo 'Number is not given';
+	}
+?>
